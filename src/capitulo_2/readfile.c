c35e4f1b9b1872387aea6f3534982db187a5d776
renzoopy <lorenzocabrera8@gmail.com>
2021-09-06 12:03:07 -0400

    Listing terminado

---
+++ src/capitulo_2/readfile.c
@@ -0,0 +1,44 @@
+#include <fcntl.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdio.h>
+char* read_from_file (const char* filename, size_t length){
+    char* buffer;
+    int fd;
+    ssize_t bytes_read;
+
+/* Allocate the buffer.  */
+    buffer = (char*) malloc (length);
+    if (buffer == NULL)
+        return NULL;
+
+/* Open the file.  */
+    fd = open (filename, O_RDONLY);
+    if (fd == -1) {
+    /* open failed.  Deallocate buffer before returning.  */
+		printf("falla");
+        free (buffer);
+        return NULL;
+    }
+
+/* Read the data.  */
+    bytes_read = read (fd, buffer, length);
+    if (bytes_read != length) {
+    /* read failed.  Deallocate buffer and close fd before returning.  */
+        free (buffer);
+        close (fd);
+        return NULL;
+    }
+
+/* Everything’s fine.  Close the file and return the buffer.  */
+    close (fd);
+    return buffer;
+}
+
+int main(){
+    char s [] = "Texto de prueba";
+    printf("%s\n", read_from_file("capitulo_2/prueba.txt", sizeof(s)));
+    return 0;
+}
